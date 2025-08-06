33f8b124c1631b39988343ee471e10892a7d45e2
Ethan Meli <etme6835@colorado.edu>
2025-08-06 16:10:42 -0700

    Add api url

--- mobile/store/authStore.js
+++ mobile/store/authStore.js
@@ -1,18 +1,19 @@
 import {create} from 'zustand';
+import "dotenv/config"; // Load environment variables
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const useAuthStore = create((set) => ({
   user: null,
   token: null,
   isLoading: false,
-
+  
   register: async (username, email, password) => {
     set({ isLoading: true });
     try {
-      const response = await fetch("https://localhost:3000/api/auth/register", {
+      const response = await fetch(`${process.env.API_URL}/api/auth/register`, {
         method: "POST",
         headers: {
-          "Content-Type": "application.json"
+          "Content-Type": "application/json"
         },
         body: JSON.stringify({
           username,
@@ -22,7 +23,7 @@ export const useAuthStore = create((set) => ({
       })
 
       const data = await response.json();
-
+      
       if (!response.ok) {
         throw new Error(data.message || "Something went wrong");
       }
