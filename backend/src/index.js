9836d04ce050a2c2ed5dd18a576dd31b31a6acf9
Ethan Meli <etme6835@colorado.edu>
2025-08-20 22:26:31 -0600

    fix limit

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,13 +10,12 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.json({ limit: "10mb" })); // Increased limit for large payloads
 app.use(cors()); // Enable CORS for all routes
 
 job.start();
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
-app.use(express.json({ limit: "10mb" })); // Increase limit as needed
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
